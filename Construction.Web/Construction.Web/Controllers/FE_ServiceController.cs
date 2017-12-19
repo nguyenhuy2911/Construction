a82e30a4b1c465d0c21552dd0a8e4fd087827961
nguyenhuy2911 <huy.nguyen.git@gmail.com>
2017-12-20 00:11:32 +0700

    commit file

---
+++ Construction.Web/Construction.Web/Controllers/FE_ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Construction.Web.Controllers
+{
+    public class FE_ServiceController : Controller
+    {
+      
+        public ActionResult HomeItems()
+        {
+            return View();
+        }
+    }
+}
